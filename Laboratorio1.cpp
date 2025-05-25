3475f2dd4d840f509d36a61875e50d1ca2bf230a
thebr0nx <23-10233@usb.ve>
2025-05-24 21:09:49 -0400

    Add files via upload

---
+++ Laboratorio1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    double num1 = 53, num2 = -27, num3 = 0.005;
+    double suma = num1 + num2 + num3;
+
+    std::cout << "La suma de " << num1 << ", " << num2 << " y " << num3 << " es: " << suma << std::endl;
+
+    return 0;
+}
